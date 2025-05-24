747757525388809539c25c20b1f55659ae26daf2
Aakash-Pandit <aakashpandit366@gmail.com>
2025-05-24 15:02:43 +0530

    updates code

--- deploy_scripts/production.sh
+++ deploy_scripts/production.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eo
-export AWS_PROFILE=reetro_golang
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 576641250044.dkr.ecr.ap-south-1.amazonaws.com
+export AWS_PROFILE=reetro_application
+aws ecr get-login-password --region ap-south-1 | docker login --username USERNAME --password-stdin PASSWORD
 
-cd reetro-golang
+cd reetro-application
 
 git checkout main
 git pull origin main
